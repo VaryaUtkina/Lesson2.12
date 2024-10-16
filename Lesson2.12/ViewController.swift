58bad778b59b665ec085af6fca4801f8ae88f719
Varya Utkina <varya2412@mail.ru>
2024-10-16 15:06:55 +0300

    Initial Commit

---
+++ Lesson2.12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson2.12
+//
+//  Created by Варвара Уткина on 16.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
